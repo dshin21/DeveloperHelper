56adf308d911092f40b31995de1b0bfe4403c38b
Evan <gmziven@gmail.com>
2019-03-26 10:51:06 -0700

    add styling todo list

--- src/components/rightBot.jsx
+++ src/components/rightBot.jsx
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
-
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Checkbox from "@material-ui/core/Checkbox";
+import Avatar from "@material-ui/core/Avatar";
 class RightBot extends Component {
   state = {};
   render() {
@@ -7,7 +15,7 @@ class RightBot extends Component {
       <div id="rightBot">
         <textarea
           style={{ width: "100%", height: "100%" }}
-          defaultValue="test"
+          defaultValue="The print function for JS is console.log();"
         />
       </div>
     );
--- src/components/rightTop.jsx
+++ src/components/rightTop.jsx
@@ -1,40 +1,82 @@
 import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Checkbox from "@material-ui/core/Checkbox";
+import TextField from "@material-ui/core/TextField";
+import Fab from "@material-ui/core/Fab";
+import { red } from "@material-ui/core/colors";
+import AddIcon from "@material-ui/icons/Add";
+const styles = theme => ({
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit
+  }
+});
 
 class RightTop extends Component {
   state = {};
+  state = {
+    checked: [0]
+  };
+
+  handleToggle = value => () => {
+    const { checked } = this.state;
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    this.setState({
+      checked: newChecked
+    });
+  };
+
   render() {
+    const { classes } = this.props;
+
     return (
       <div id="rightTop">
-        <ul className="list" style={{ listStyleType: "none" }}>
-          <li className="list-item">
-            <input type="checkbox" />
-            to-do <button>Del</button>
-          </li>
-          <li className="list-item">
-            {" "}
-            <input type="checkbox" />
-            to-do
-            <button>Del</button>
-          </li>
-          <li className="list-item">
-            {" "}
-            <input type="checkbox" />
-            to-do <button>Del</button>
-          </li>
-          <li className="list-item">
-            {" "}
-            <input type="checkbox" />
-            to-do <button>Del</button>
-          </li>
-          <li className="list-item">
-            {" "}
-            <input type="checkbox" />
-            to-do <button>Del</button>
-          </li>
-        </ul>
+        <List className={"a"}>
+          {[0, 1, 2, 3].map(value => (
+            <ListItem key={value} role={undefined} dense button>
+              <Checkbox
+                checked={this.state.checked.indexOf(value) !== -1}
+                tabIndex={-1}
+                disableRipple
+                onClick={this.handleToggle(value)}
+              />
+
+              <TextField
+                id="standard-bare"
+                className={classes.textField}
+                defaultValue="Bare"
+                margin="normal"
+                fullWidth="true"
+              />
+              {/* <ListItemText primary={`Line item ${value + 1}`} /> */}
+              {/* <ListItemSecondaryAction>
+              <IconButton aria-label="Comments">
+                <CommentIcon />
+              </IconButton>
+            </ListItemSecondaryAction> */}
+            </ListItem>
+          ))}
+        </List>
+        <Fab
+          style={{ padding: 0, margin: 0 }}
+          className={"fab"}
+          color="secondary"
+        >
+          <AddIcon />
+        </Fab>
       </div>
     );
   }
 }
-
-export default RightTop;
+// export default RightTop;
+export default withStyles(styles)(RightTop);
